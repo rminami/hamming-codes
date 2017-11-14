30ec46298fa1a66538b902813f435867d121291a
Ryosuke Minami <ryosukem13@gmail.com>
2017-11-14 04:06:03 +0000

    tabs -> spaces

--- hamming-app/main.py
+++ hamming-app/main.py
@@ -8,33 +8,33 @@ import numpy as np
 
 
 def rand_array(size):
-	arr = np.empty(size, dtype=np.int)
-	for i in range(size):
-		arr[i] = randint(0, 1)
-	return arr
+    arr = np.empty(size, dtype=np.int)
+    for i in range(size):
+        arr[i] = randint(0, 1)
+    return arr
 
 def main():
 
-	size = 4
-	encoder = HammingEncoder(size)
-	checker = HammingChecker(size)
+    size = 4
+    encoder = HammingEncoder(size)
+    checker = HammingChecker(size)
 
-	word = rand_array(size)
-	coded = encoder.encode(word)
+    word = rand_array(size)
+    coded = encoder.encode(word)
 
-	checker.check_print(coded) 		# No bits should be corrupted yet
-	# TODO bit 6 is detected as corrupted when word = [1 0 1 1]
+    checker.check_print(coded)      # No bits should be corrupted yet
+    # TODO bit 6 is detected as corrupted when word = [1 0 1 1]
 
-	print(str(word) + " has been encoded into: " + str(coded))
+    print(str(word) + " has been encoded into: " + str(coded))
 
-	for i in range(7):
-		coded[i] = (coded[i] + 1) % 2	# Flip a bit
-		print("Bit " + str(i) + " has been flipped: " + str(coded) + " -> corrects to: " + str(checker.correct(coded.copy())))
-		coded[i] = (coded[i] + 1) % 2	# Flip it back
+    for i in range(7):
+        coded[i] = (coded[i] + 1) % 2   # Flip a bit
+        print("Bit " + str(i) + " has been flipped: " + str(coded) + " -> corrects to: " + str(checker.correct(coded.copy())))
+        coded[i] = (coded[i] + 1) % 2   # Flip it back
 
-	# checker.check_print(coded)		# Checker should detect corrupted bit
+    # checker.check_print(coded)        # Checker should detect corrupted bit
 
 
 # Main launcher
 if __name__ == '__main__':
-	main()
+    main()
--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-	
-	name='hamming-app',
-	packages=['flask-app', 'hammingclasses'],
-	include_package_data=True,
-	install_requires=[
-		'flask',
-		'numpy'
-	],
+    
+    name='hamming-app',
+    packages=['flask-app', 'hammingclasses'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+        'numpy'
+    ],
 )
